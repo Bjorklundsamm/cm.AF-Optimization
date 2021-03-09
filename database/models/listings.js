3490d7a5ad997a70ffe6bd42a9586e0de9bdcc58
Bjorklundsamm <bjorklundsamm@gmail.com>
2021-03-09 13:20:55 -0800

    Formatted layout, initialized processing of README

--- database/models/listings.js
+++ database/models/listings.js
@@ -1,13 +1,13 @@
 const DB = require('../db');
 
 const getAll = (callback) => {
-  const query = 'SELECT home_id, street_address, zip_code, county, city, cost FROM listings LIMIT 25';
+  const query = 'SELECT home_id, street_address, zip_code, county, city, cost FROM houses LIMIT 100';
   DB.execute(query)
     .then((result) => callback(result.rows));
 };
 
 const getById = (params, callback) => {
-  const query = 'SELECT * FROM home_data WHERE home_id=?';
+  const query = 'SELECT * FROM houses WHERE home_id=?';
   DB.execute(query, params)
     .then((result) => callback(result.rows[0]));
 };
